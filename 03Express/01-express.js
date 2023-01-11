21a8321beae839a41f4e378a7c8a00fc43ebe792
pepeamores <joseamores.20@campuscamara.es>
2023-01-11 19:14:44 +0100

    fin

--- 03Express/01-express.js
+++ 03Express/01-express.js
@@ -8,19 +8,13 @@ app.set('view engine' ,'ejs')
 //middleware
 app.use(express.static(__dirname + '/public'));
 
-//peticiones basicas HTTP
-app.get('/', (req, res) => {
- // res.send('Hello Node.js!')
-  res.render("contacto", {tituloContacto : "estamos en contacto de forma dinámico"})
-})
-
-app.get('/contacto', (req, res) => {
-    res.send('Estás en contacto')
-})
+//Llamadas a las rutas:
+app.use('/', require('./router/rutas'));
+app.use('/pokemon', require('./router/pokemon'));
 app.use((req,res) => {
     res.status(404).sendFile(__dirname + "/public/404.html")
    })
-   
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
