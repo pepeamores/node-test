f1e9345717c37a41697736da96512f2fb35e9186
pepeamores <joseamores.20@campuscamara.es>
2023-01-16 16:40:44 +0100

    nodeDB

--- 03Express/01-express.js
+++ 03Express/01-express.js
@@ -1,6 +1,25 @@
 const express = require('express')
 const app = express()
-const port = 3005
+require ('dotenv').config()
+const port = process.env.PORT||3005
+//Conexión a base de datos
+const mongoose = require('mongoose');
+
+//Variables que tendremos siempre:
+//Lo correcto será declararlas EN VARIABLES DE ENTORNO
+//para que nadie vea directamente nuestras credenciales
+
+/*const user = 'cursonode';
+const password = 'h5iov5yzJermGENG';
+const dbname = 'dbpokemon';
+*/
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wke4kik.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`; //URL de conexión, que completaremos luego
+mongoose.connect(uri,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+)
+  .then(() => console.log('Base de datos conectada'))
+  .catch(e => console.log(e))
+
 //motor de plantilla
 app.set('views',__dirname + '/views');
 app.set('view engine' ,'ejs')
